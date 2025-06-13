d7e77e7fe49ff57b15a8cefe479b74780492e0c6
Vaibhav Sanjay Ambekar <88874041+A-Vaibhav@users.noreply.github.com>
2025-06-13 11:38:05 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, welcome to the Frontend Application!</h1>
+      <p>API Status: <a href="/api" target="_blank">Click to check</a></p>
+    </div>
+  );
+}
+
+export default App;
